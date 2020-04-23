c561482ad3cbeb0047f187b961bb45ceaa7b3a96
James Rogers <jarogers095@gmail.com>
2020-04-23 05:30:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     user = User.find_by(username: params["username"], password: params["password"])
+    puts user
     if user != nil
       session[:user_id] = user.id
       redirect to "/account"
